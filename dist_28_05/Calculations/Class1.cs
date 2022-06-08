31ce9099d531705204638ce086c630757810107a
ultraniu8m <95349360+ultraniu8m@users.noreply.github.com>
2022-06-08 21:33:31 +0300

    Add files via upload

---
+++ dist_28_05/Calculations/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calculations1
+{
+    public class Calculations
+    {
+        public string AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+
+            TimeSpan common = new TimeSpan(0, consultationTime, 0);
+            int get_isklucheniya = 0;
+            while (beginWorkingTime <= (endWorkingTime - common))
+            {
+
+                if (beginWorkingTime == startTimes[get_isklucheniya])
+                {
+                    Console.WriteLine("got you");
+                    beginWorkingTime += TimeSpan.FromMinutes(durations[get_isklucheniya]);
+                    string local_res = (beginWorkingTime + "-" + (beginWorkingTime + common));
+                    beginWorkingTime += common;
+                    get_isklucheniya += 1;
+                    return local_res;
+                }
+                else
+                {
+                    Console.WriteLine(beginWorkingTime + "-" + (beginWorkingTime + common));
+                    string local_res = (beginWorkingTime + "-" + (beginWorkingTime + common));
+                    beginWorkingTime += common;
+                    return local_res;
+                }
+
+            }
+
+            return "Completed";
+        }
+    }
+}
---
+++ dist_28_05/dist_28_05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Calculations1;
+namespace dist_28_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculations a = new Calculations();
+            TimeSpan[] startTimes = { new TimeSpan(9, 0, 0), new TimeSpan(10, 0, 0), new TimeSpan(17,0,0) };
+            int[]  time_ = { 30, 20 };
+            TimeSpan beginWorkingTime = new TimeSpan(8, 0, 0);
+            TimeSpan endWorkingTime = new TimeSpan(16, 0,0);
+            TimeSpan common = new TimeSpan(0, 30, 0);
+            int get_isklucheniya = 0;
+            double hours = (endWorkingTime - beginWorkingTime).TotalHours;
+            while (beginWorkingTime <= (endWorkingTime-common))
+            {
+                
+                if (beginWorkingTime == startTimes[get_isklucheniya])
+                {
+                    Console.WriteLine("got you");
+                    beginWorkingTime += TimeSpan.FromMinutes(time_[get_isklucheniya]);
+                    Console.WriteLine(beginWorkingTime + "-" + (beginWorkingTime + common));
+                    beginWorkingTime += common;
+                    get_isklucheniya += 1;
+                }
+                else
+                {
+                    Console.WriteLine(beginWorkingTime + "-" + (beginWorkingTime + common));
+                    beginWorkingTime += common;
+                }
+                
+            }
+                
+        }
+    }
+}
